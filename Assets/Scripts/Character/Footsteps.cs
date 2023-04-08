b080b4ec7c7a8b2880ae4ff2159dd23b330318ae
Orobosa Ogbemudia <55265742+aardig100@users.noreply.github.com>
2023-04-08 17:57:37 -0500

    Ambient noise and footsteps for player

---
+++ Assets/Scripts/Character/Footsteps.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    CharacterController cc;
+    AudioSource faudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        faudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cc.velocity.magnitude > 2f && faudio.isPlaying == false)
+        {
+            faudio.volume = Random.Range(0.8f, 1);
+            faudio.pitch = Random.Range(0.8f, 1.1f);
+            faudio.Play();
+        }
+    }
+}
