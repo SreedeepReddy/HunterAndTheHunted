9950ffe6c918c4b6a7ac3ca074fec8a43316d90a
Sreedeep Reddy <sreedeepreddyk@gmail.com>
2023-04-08 19:48:23 -0500

    Fixed Character Selection and Spawn Coordinates

--- Assets/Scripts/Menus/OpenCharacterMenu.cs
+++ Assets/Scripts/Menus/OpenCharacterMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,14 +10,28 @@ public class OpenCharacterMenu : MonoBehaviour
     public GameObject reticle;
     public EventSystem eventSystem;
     public GameObject firstSelected;
+    public GameObject vrGroup;
+    public GameObject charMenu;
+    private bool initCharMenu = true;
 
-    private void Start()
+    private void InitCharMenu()
     {
+        charMenu.SetActive(true);
         character.GetComponent<CharacterMovement>().speed = 0;
         reticle.SetActive(false);
         eventSystem.GetComponent<XRCardboardInputModule>().enabled = false;
         eventSystem.GetComponent<StandaloneInputModule>().enabled = true;
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(firstSelected);
+        initCharMenu = false;
     }
+
+    private void Update()
+    {
+        if (vrGroup.activeSelf && initCharMenu) 
+        {
+            InitCharMenu();
+        }
+    }
+
 }
