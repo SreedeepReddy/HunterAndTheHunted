9c13ca838670fb2145dd078aebcfa49a0426d784
Orobosa Ogbemudia <55265742+aardig100@users.noreply.github.com>
2023-05-03 00:01:11 -0500

    Basic working roaming ai

--- Assets/Scripts/Character/Footsteps.cs
+++ Assets/Scripts/Character/Footsteps.cs
@@ -1,26 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Footsteps : MonoBehaviour
 {
     CharacterController cc;
     AudioSource faudio;
+    [SerializeField] bool isNpc;
+    NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
         cc = GetComponent<CharacterController>();
         faudio = GetComponent<AudioSource>();
+        if (isNpc)
+            agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(cc.isGrounded == true && cc.velocity.magnitude > 2f && faudio.isPlaying == false)
+        if (isNpc)
         {
-            faudio.volume = Random.Range(0.8f, 1);
-            faudio.pitch = Random.Range(0.8f, 1.1f);
-            faudio.Play();
+            if (agent.velocity.magnitude > 2f && faudio.isPlaying == false)
+            {
+                faudio.volume = Random.Range(0.8f, 1);
+                faudio.pitch = Random.Range(0.8f, 1.1f);
+                faudio.Play();
+            }
         }
+        else
+        {
+            if (cc.isGrounded == true && cc.velocity.magnitude > 2f && faudio.isPlaying == false)
+            {
+                faudio.volume = Random.Range(0.8f, 1);
+                faudio.pitch = Random.Range(0.8f, 1.1f);
+                faudio.Play();
+            }
+        }
+        
     }
 }
